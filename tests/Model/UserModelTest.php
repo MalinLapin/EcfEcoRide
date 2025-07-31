8b4893fc1940293c06ec68ac8f651a0deb550876
Marc <marc.uny39@gmail.com>
2025-07-31 17:54:55 +0200

    feat: création d'un dossier test réaliser avec phpUnit. Ici nous testons le UserModel.php

---
+++ tests/Model/UserModelTest.php
@@ -0,0 +1,24 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Model\UserModel;
+
+class UserModelTest extends TestCase
+{
+    public function testHydrateSetsProperties()
+    {
+        $data = [
+            'id_user' => 1,
+            'last_name' => 'Uny',
+            'first_name' => 'Marc',
+            'created_at' => '2023-06-01 12:00:00',
+            'role' => 'User'
+        ];
+
+        $user = UserModel::createAndHydrate($data);
+
+        $this->assertEquals(1, $user->getIdUser());
+        $this->assertEquals('Uny', $user->getLastName());
+        $this->assertInstanceOf(DateTimeImmutable::class, $user->getCreatedAt());
+        $this->assertEquals('User', $user->getRole()->value);
+    }
+}
