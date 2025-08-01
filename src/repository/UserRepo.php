e798adca9dfd1f498788518caa85973b98ca4ec3
Marc <marc.uny39@gmail.com>
2025-08-01 15:51:32 +0200

    fix: création de commentaire PhpDoc

--- src/repository/UserRepo.php
+++ src/repository/UserRepo.php
@@ -8,7 +8,16 @@ use App\model\UserModel;
 
 class UserRepo extends Repository
 {
-    //1.Méthode pour récuperer un utilisateur par son pseudo
+    protected string $tableName = 'user';
+    
+    /**
+     * Récupère un utilisateur par son pseudo.
+     * @param string $pseudo Le pseudo de l'utilisateur à récupérer.
+     * @return UserModel|null L'instance de UserModel correspondant à l'utilisateur, ou null si l'utilisateur n'existe pas.
+     * 
+     * Cette méthode prépare une requête SQL pour récupérer un utilisateur par son pseudo.
+     * Elle utilise une exception pour signaler si l'utilisateur n'est pas trouvé.
+     */
     public function getUserByPseudo(string $pseudo): ?UserModel
     {
         $query = "SELECT * FROM {$this->tableName} WHERE pseudo = :pseudo";
@@ -23,7 +32,11 @@ class UserRepo extends Repository
         }        
     }
     
-    //2.Méthode pour récuperer un utilisateur par son email
+    /**
+     * Récupère un utilisateur par son adresse e-mail.
+     * @param string $email L'adresse e-mail de l'utilisateur à récupérer.
+     * @return UserModel|null L'instance de UserModel correspondant à l'utilisateur, ou null si l'utilisateur n'existe pas.
+     */
     public function getUserByEmail(string $email): ?UserModel
     {
         $query = "SELECT * FROM {$this->tableName} WHERE email = :email";
