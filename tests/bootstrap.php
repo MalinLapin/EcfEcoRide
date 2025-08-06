30dcdbf781fc09df5b68496b6cb87fa4dc6baa5b
Marc <marc.uny39@gmail.com>
2025-08-07 00:25:52 +0200

    feat: création de test unitaire pour le repo de Ridesharing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
 // Charger l'autoload de Composer
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use App\config\Config;
+use App\Config\Config;
 
 //Charger un fichier de configuration spécifique pour les tests afin d'utiliser une base de données séparer
 //Cela évite de polluer la base de données de développement.
