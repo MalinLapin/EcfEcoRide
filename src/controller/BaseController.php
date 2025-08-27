dc6c068e4d3ee87473c844faf7b5af25e211daaa
Marc <marc.uny39@gmail.com>
2025-08-27 17:41:12 +0200

    feat: création d'un controller de base pour la redirection d'Url + recupération des données d'une requête POST + une vérification d'authentification d'utilisateur

---
+++ src/controller/BaseController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\controller;
+
+use App\security\Validator;
+use App\utils\Response;
+
+/**
+ * Controler de base
+ * Toutes les autres classe de controlleru hériteront de celle ci
+ * 
+ */
+abstract class BaseController
+{
+    protected Response $response;
+    protected Validator $validator;
+
+    public function __construct()
+    {
+        $this->response = new Response();
+        $this->validator = new Validator();
+    }
+
+    /**
+     * Affiche une vue en l'injectant dans le layout principale
+     * @param string $view le nom de fichier de vue
+     * @param array $data les données à rendres accéssible dans la vue
+     */
+    protected function render(string $view, array $data = []):void
+    {
+        // On construit le chemin complet vers le chemin de vue
+        $viewPath = dirname(__DIR__, 2).'/views/'.$view.'.php';
+        // On vérifie que le fichier vue existe bien.
+        if(!file_exists($viewPath))
+        {
+            $this->response->error("Vue non trouvée : $viewPath", 500);
+            return;
+        }
+
+        // extract() Transforme les clefs d'un tableau en variables
+        // Exemple: $data = ['title'=>'Accueil']  devient $title = 'Accueil'
+        extract($data);
+
+        // On utilise la mise en tampon de sortie (outpout buffering) pour capturer le HTML de la vue.
+        ob_start();
+        include $viewPath;
+
+        // Ici on vide le cache, la variable $content contient la vue
+        $content = ob_get_clean();
+
+        // Finalement, on inclut le layout principal, qui peut maintenant utiliser la variable $content.
+        include dirname(__DIR__, 2).'/views/layout.php';
+    }
+
+    /**
+     * Récupère et nettoie les données envoyées via une requête POST
+     */
+    protected function getPostData():array
+    {
+        return$this->validator->sanitize($_POST);
+    }
+
+    /**
+     * Vérifie si l'utilisateur est connecter sinon le redirige vers la page login.
+     */
+    protected function requireAuth():void
+    {
+        if(!isset($_SESSION['id_user']))
+        {
+            $this->response->redirect('/login');
+        }
+    }
+}
