0e8f373112e433b0c0c52e572b2a21b719de62fc
Marc <marc.uny39@gmail.com>
2025-10-02 23:28:41 +0200

    fix: correction de plusieus soucis au niveau des id du <Form> + feat: ajout de partie dynamique en Php telle que les erreurs à afficher

--- src/view/template/register.php
+++ src/view/template/register.php
@@ -1,30 +1,38 @@
-
-<main class="mainContent">
-    <section class="presentationSection">
-        <h2 class="montserratBold titleColor">EcoRide</h2>
-    </section>
-    <div class="card">
+<section class='register'>
+    <div class="presentationSection">
+            <h1 class="montserratBold titleColor">EcoRide,<br>votre covoiturage <br> éco-responsable.</h1>
+        </div>
+    <div class="card robotoRegular">
         <h3 class="robotoBold">Inscription</h3>
-        <form method="POST" action="../../controller/AuthController.php/register">
+        <form method="POST" action="/register">
             <div class="form-group">
-                <label for="pseudo" class="robotoRegular"></i>Pseudo :</label>
+                <label for="pseudo"></i>Pseudo :</label>
                 <input type="text" id="pseudo" name="pseudo" placeholder="Votre pseudo" required>
             </div>
             <div class="form-group">
-                <label for="pseudo" class="robotoRegular">Email:</label>
-                <input type="text" id="pseudo" name="pseudo" placeholder="Votre mail" required>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Votre mail" required>                
             </div>
             <div class="form-group">
-                <label for="pseudo" class="robotoRegular">Mot de passe sécuriser :</label>
-                <input type="text" id="pseudo" name="pseudo" placeholder="Votre mot de passe" required>
+                <label for="password">Mot de passe sécuriser :</label>
+                <input type="password" id="password" name="password" placeholder="Votre mot de passe" required>
             </div>
             <div class="form-group">
-                <label for="password" class="robotoRegular">Confirmer :</label>
-                <input type="password" id="password" name="password" placeholder="Confirmer le mot de passe" required>
+                <label for="confirmPassword">Confirmer :</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirmer le mot de passe"
+                    required>
             </div>
-            <button type="submit" class="robotoRegular">Monter à bord !</button>
-            <div class="message robotoRegular">Déjà un comtpe ? <a href="../../view/template/login.php">Connexion</a></div>
+            <div class='errorsList'>
+                    <ul>
+                        <?php if(isset($errors) && $errors){
+                        foreach ($errors as $error):?>
+                            <li class='error robotoBold'><?=$error?></li>                                    
+                        <?php endforeach;}?>
+                    </ul>    
+                </div>
+            <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrf_token) ?>">
+            <button type="submit">Monter à bord !</button>
+            <div class="message">Déjà un comtpe ? <a href="/login">Connexion</a></div>
         </form>
-    </div>   
-</main>
-
+    </div>
+</section>
