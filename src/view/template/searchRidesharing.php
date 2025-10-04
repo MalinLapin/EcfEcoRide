1227348280e408c71de019b019db0e0c9c3e37d0
Marc <marc.uny39@gmail.com>
2025-10-04 15:54:38 +0200

    refact(searchRidesharing): Ajout de la class de police dans la balise général pour éviter la redondance d'appel + modification du nom de la section pour une meilleure* compréhension.

--- src/view/template/searchRidesharing.php
+++ src/view/template/searchRidesharing.php
@@ -1,46 +1,55 @@
-<section class="searchSection">
+<section class="searchRidesharing robotoBold">
         <h2 class="montserratBold titleColor">Trouver votre covoiturage</h2>
         <div class="searchContent">
-            <form method="POST" action="#" class='searchForm'>
+            <form method="POST" action="/search" class='searchForm'>
                 <div class='departureCity'>
-                    <label for="departureCity" class="robotoBold">Ville de départ :</label>
+                    <label for="departureCity">Ville de départ :</label>
                     <input type="text" name="departureCity" id="departureCity" placeholder="Ville" required>
                 </div>
                 <div class="departureAddress">
-                    <label for="departureAddress" class="robotoBold">Adresse de départ :</label>
+                    <label for="departureAddress">Adresse de départ :</label>
                     <input type="text" name="departureAddress" id="departureAddress" placeholder="CP, rue">
                 </div>
                 <div class='arrivalCity'>
-                    <label for="arrivalCity" class="robotoBold">Ville d'arriver :</label>
+                    <label for="arrivalCity">Ville d'arriver :</label>
                     <input type="text" name="arrivalCity" id="arrivalCity" placeholder="Ville" required>
                 </div>
                 <div class="arrivalAddress">
-                    <label for="arrivalAddress" class="robotoBold">Adresse d'arriver :</label>
+                    <label for="arrivalAddress">Adresse d'arriver :</label>
                     <input type="text" name="arrivalAddress" id="arrivalAddress" placeholder="CP, rue">
                 </div>
                 <div class="departureDate">
-                    <label for="departureDate" class="robotoBold">Date/heure :</label>
+                    <label for="departureDate">Date/heure :</label>
                     <input type="datetime-local" name="departureDate" id="departureDate" required>
                 </div>
                 <div class="nbSeats">
-                    <label for="nbSeats" class="robotoBold">Place :</label>
+                    <label for="nbSeats">Place :</label>
                     <input type="number" name="nbSeats" id="nbSeats" value="1" max='6'>
                 </div>
                 <div class="pricePerSeat">
-                    <label for="pricePerSeat" class="robotoBold">Prix de la place</label>
+                    <label for="pricePerSeat">Prix de la place</label>
                     <input type="number" name="pricePerSeat" id="pricePerSeat">
                 </div>
                 <div class="gradeDriver">
-                    <label for="gradeDriver" class="robotoBold">Note du conducteur</label>
+                    <label for="gradeDriver">Note du conducteur</label>
                     <input type="number" name="gradeDriver" id="gradeDriver" min='1' max='5'>
                 </div>
                 <div class="energyType">
-                    <label for="energyType"class="robotoBold">Trajet électrique uniquement ?</label>
+                    <label for="energyType">Trajet électrique uniquement ?</label>
                     <input type="checkbox" name="energyType" id="energyType">
+                </div>
+                <div class='errorsList'>
+                    <ul>
+                        <?php if(isset($errors) && $errors){
+                        foreach ($errors as $error):?>
+                            <li class='error robotoBold'><?=$error?></li>                                    
+                        <?php endforeach;}?>
+                    </ul>    
                 </div>
 
                 <div>
-                    <button type="submit" class="btnSearch robotoBold">C'est partie!</button>
+                    <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrf_token) ?>">
+                    <button type="submit" class="btnSearch">C'est partie!</button>
                 </div>
             </form>
         </div>
