5cf44ff95199fc804230488856463013d3092a20
Marc <marc.uny39@gmail.com>
2025-10-04 22:44:43 +0200

    feat(ridesharingCard): Ajout de dynamisme dans notre template.

--- src/view/template/ridesharingCard.php
+++ src/view/template/ridesharingCard.php
@@ -1,30 +1,32 @@
 <?php
-    $ride = $listRidesharing['ridesharing'];
-    $driver = $listRidesharing['driverInfo'];
-    $carEnergyType = $listRidesharing['carEnergyType'];
+        $ride = $ridesharing['ridesharingInfo'];
+        $driver = $ridesharing['driverInfo'];
+        $carEnergyType = $ridesharing['carEnergyType'];    
 ?>
 
 <article class="ridesharingCard">
     <div class=infoRidesharing>
         <div class='infoDriver'>
             <div class='energyType'>
-                <img src="../assets/images/electric_bolt_24dp.png" alt="icone carburant">
+                <?php if($carEnergyType == "electric"):?>
+                <img src="../assets/images/electric_bolt_24dp.png" alt="icone carburant" class="electricRide">
+                <?php endif; ?>
             </div>
             <div class='driver robotoBold'>
                 <img src="" alt="photo">
-                <p><?=$ride->getPseudo()?></p>
-                <p><?=$ride->getGrade()?></p>  
+                <p><?=$driver->getPseudo()?></p>
+                <p><?=$driver->getGrade()?></p>  
             </div>
         </div>
         <div class='infoDrive'>
             <div class='ridesharing robotoBold'>
-                <p>Lyon, <br><span class='robotoRegular'>Part Dieu</span></p>
-                <p>Saint Etienne <br><span class='robotoRegular'>Rue du test</span></p>
+                <p><?= $ride->getDepartureCity()?> <br><span class='robotoRegular'><?= $ride->getDepartureAddress()?></span></p>
+                <p><?= $ride->getArrivalCity()?> <br><span class='robotoRegular'><?= $ride->getArrivalAddress()?></span></p>
             </div>
             <div class='otherInfo robotoBold'>
-                <p>16h45</p>
-                <p><span> 8 </span> Cr.</p>
-                <p><span> 5 </span>places</p>
+                <p class="departureDate"><?= $ride->getDepartureDate()->format('Y-m-d H:i')?></p>
+                <p><span> <?= $ride->getPricePerSeat()?> </span> Cr.</p>
+                <p><span> <?= $ride->getAvailableSeats()?> </span>places</p>
             </div>
         </div>
     </div>
