8ac741a0d319163dd1ada3fb0b1dabab9f80bc77
Marc <marc.uny39@gmail.com>
2025-10-04 15:52:59 +0200

    feat(ridesharingCard): Ajout de dynamisme dans le pseudo du conducteur.

--- src/view/template/ridesharingCard.php
+++ src/view/template/ridesharingCard.php
@@ -1,26 +1,32 @@
-<div class="ridesharingCard">
+<?php
+    $ride = $listRidesharing['ridesharing'];
+    $driver = $listRidesharing['driverInfo'];
+    $carEnergyType = $listRidesharing['carEnergyType'];
+?>
+
+<article class="ridesharingCard">
     <div class=infoRidesharing>
         <div class='infoDriver'>
             <div class='energyType'>
                 <img src="../assets/images/electric_bolt_24dp.png" alt="icone carburant">
             </div>
-            <div class='driver'>
-                <img src="" alt="photo du conducteur">
-                <p class='robotoBold'>testPseudo</p>
-                <p class='robotoBold'>4/5</p>  
+            <div class='driver robotoBold'>
+                <img src="" alt="photo">
+                <p><?=$ride->getPseudo()?></p>
+                <p><?=$ride->getGrade()?></p>  
             </div>
         </div>
         <div class='infoDrive'>
-            <div class='ridesharing'>
-                <p class='robotoBold'>Lyon, <br><span class='robotoRegular'>Part Dieu</span></p>
-                <p class='robotoBold'>Saint Etienne <br><span class='robotRegular'> Rue du test</span></p>
+            <div class='ridesharing robotoBold'>
+                <p>Lyon, <br><span class='robotoRegular'>Part Dieu</span></p>
+                <p>Saint Etienne <br><span class='robotoRegular'>Rue du test</span></p>
             </div>
-            <div class='otherInfo'>
-                <p class='robotoBold'>16h45</p>
-                <p class='robotoRegular'><span class='robotoBold'> 8 </span> Cr.</p>
-                <p class='robotoRegular'><span class='robotoBold'> 5 </span>places</p>
+            <div class='otherInfo robotoBold'>
+                <p>16h45</p>
+                <p><span> 8 </span> Cr.</p>
+                <p><span> 5 </span>places</p>
             </div>
         </div>
     </div>
-    <button class='robotoBold'>Détails</button>
-</div>
+    <button class='robotoBold detailBtn'>Détails</button>
+</article>
