d4e5b1652fbcee7f7439c2e47ebfc89e8ae4d7a4
Marc <marc.uny39@gmail.com>
2025-10-20 21:04:55 +0200

    refact(PreferenceRepo): Modification de la recherche de préférence qui se fait par trajet et non par voiture.

--- src/repository/PreferenceRepo.php
+++ src/repository/PreferenceRepo.php
@@ -8,10 +8,10 @@ class PreferenceRepo extends BaseRepoMongo
     protected string $collectionName = 'preference';
     protected ?string $className = PreferenceModel::class;
 
-    // Recherche une préférences par son identifiant de voiture
-    public function findByCar(int $idCar): array
+    // Recherche une préférences par l'identifiant du covoiturage
+    public function findByRidesharing(int $idRidesharing): array
     {
-        $cursor = $this->collection->find(['id_car' => $idCar]);
+        $cursor = $this->collection->find(['id_car' => $idRidesharing]);
         $out = [];
         foreach ($cursor as $doc) {
             $out[] = $this->toModel($doc);
