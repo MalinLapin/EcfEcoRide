c30be6b5857129ad51ee13664c3918b75be94eb5
Marc <marc.uny39@gmail.com>
2025-10-28 23:29:42 +0100

    feat(mentionLegal): Création du template affichant les mentions légales ainsi que le fichier Css correspondant.

---
+++ src/view/template/mentionLegal.php
@@ -0,0 +1,37 @@
+<section class="mentionSection robotoRegular">
+    <div class="titleMention">
+        <h2 class="montserratBold titleColor">Mentions Légales</h2>
+        <p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site Ecoride l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+    </div>    
+
+    <div class="edition">
+        <h3 class="montserratBold titleColor">Edition du site</h3>
+        <p>Le présent site, accessible à l’URL www.Ecf-ecoride.fr (le « Site »), est édité par : <br> Marc Uny, résidant 12 rue du vide 01000 Bourg-en-Bresse, de nationalité Française (France), né(e) le 15/02/1994, </p>
+    </div>
+
+    <div class="hebergement">
+        <h3 class="montserratBold titleColor">Hébergement</h3>
+        <p>Le Site est hébergé par la société Heroku, situé 2 Rue de l'échappée 75010 PARIS, (contact téléphonique ou email : +33304050607).</p>
+    </div>
+
+    <div class="publication">
+        <h3 class="montserratBold titleColor">Directeur de publication</h3>
+        <p>Le Directeur de la publication du Site est Marc Uny.</p>
+    </div>
+
+    <div class="contact">
+        <h3 class="montserratBold titleColor">Nous contacter</h3>
+        <ul>
+            <li>Par téléphone : +33667990380</li>
+            <li>Par email : marc.uny@orange.fr</li>
+            <li>Par courrier : 12 rue du vide 01000 Bourg en Bresse</li>
+        </ul>
+    </div>
+
+    <div class="donneesPersonnelles">
+        <h3 class="montserratBold titleColor">Données personnelles</h3>
+        <p>Le traitement de vos données à caractère personnel est régi par notre Charte du respect de la vie privée, disponible depuis la section "Charte de Protection des Données Personnelles", conformément au Règlement Général sur la Protection des Données 2016/679 du 27 avril 2016 («RGPD»).</p>
+    </div>
+
+    <p>Génération des mentions légales par Legalstart.</p>
+</section>
