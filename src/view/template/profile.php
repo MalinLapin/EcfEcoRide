041b59034c3d7a1d3dab303473a3c8cbf3abb7b3
Marc <marc.uny39@gmail.com>
2025-10-30 00:13:11 +0100

    feat(profile): Création de l'espace utilisateur.

---
+++ src/view/template/profile.php
@@ -0,0 +1,43 @@
+<section class='profileSection'>
+    <div class="titleSection">
+        <h2 class="montserratBold titleColor">Mon profil.</h2>
+    </div>
+    <div class='navBarProfil'>
+        <a href="/profile">Mon compte</a>
+        <a href="/myRidesharing">Mes trajets</a>
+    </div>
+    <div class='userCard'>
+        <div class ='picture'>
+            <img src="" alt="photo de profil">
+        </div>
+        <div class='userInfo'>
+            <p class='pseudo'>testPseudo</p>
+            <p class='creationDate'>29 octobre 2025</p>
+            <p class='creditBalance'>35 crédits</p>
+        </div>
+    </div>
+    <div class="myCars">
+        <ul>
+            <li>
+                <span class="material-symbols-outlined">directions_car</span>
+                <p>Dacia Duster</p>
+                <p>4 places</p> 
+                <span class="material-symbols-outlined">edit</span>
+            </li>
+            <li>
+                <span class="material-symbols-outlined">directions_car</span>
+                <p>Audi e-tron</p>
+                <p>1 place</p>
+                <span class="material-symbols-outlined">electric_bolt</span>
+                <span class="material-symbols-outlined">edit</span>
+            </li>
+        </ul>
+        <a href="addCar">
+            <span class="material-symbols-outlined">directions_car</span>
+            <span class="material-symbols-outlined">add</span>
+        </a> 
+    </div>
+    <div class='newRide'>
+        <a href="/showCreateRidesharing" class="newRideBtn">Proposer un trajet.</a>
+    </div>
+</section> 
