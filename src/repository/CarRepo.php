d5d0237be9fb6900eb644adada786024b601b1cd
Marc <marc.uny39@gmail.com>
2025-10-30 14:37:04 +0100

    feat(CarRepo): Création du repo de la classe CarModel afin de rechercher une liste de voitures en fonction d'un utilisateur.

---
+++ src/repository/CarRepo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\repository;
+
+use App\model\CarModel;
+use App\model\BrandModel;
+
+
+class CarRepo extends BaseRepoSql
+{
+    protected string $tableName = 'car';
+    protected string $className = CarModel::class;
+
+    public function findListCarByUserId(int $idUser): ?array
+    {
+        var_dump($idUser);
+        $query = "SELECT c.*,
+                        b.label AS brand_label
+                    FROM {$this->tableName} c
+                    JOIN brand b ON c.id_brand = b.id_brand
+                    JOIN user u ON c.id_user = u.id_user
+                    WHERE u.id_user = :id_user";
+
+        $stmt = $this->pdo->prepare($query);
+        $stmt -> bindValue(':id_user', $idUser);
+
+        var_dump($stmt);
+        $stmt -> execute();
+
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        
+        var_dump($result);
+        if($result)
+        {    
+            foreach ($result as $row) 
+            {
+                $carData = [];
+                $brandData = [];
+                
+                foreach ($row as $key => $value)
+                {
+                    if (str_starts_with($key, 'brand_')) {
+                        $brandData[substr($key, 6)] = $value;
+                    } else {
+                        $carData[$key] = $value;
+                    }
+                }
+                
+                // Créer et assembler les objets
+                $brandInfo = BrandModel::createAndHydrate($brandData);
+                var_dump($brandInfo);
+
+                $carInfo = CarModel::createAndHydrate($carData);  
+                var_dump($carInfo);        
+
+                $carList[] = [
+                    'carInfo'=>$carInfo,
+                    'brandInfo'=>$brandInfo,
+                ];
+            }
+            return $carList;
+        }
+        return null;
+    }
+}
