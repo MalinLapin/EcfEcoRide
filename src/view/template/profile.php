955964ef14b4f0319aa8b9e51481fff41ed16a5f
Marc <marc.uny39@gmail.com>
2025-10-30 11:44:39 +0100

    feat(profile.css): Création de la feuille de style du template profile.

--- src/view/template/profile.php
+++ src/view/template/profile.php
@@ -1,11 +1,17 @@
-<section class='profileSection'>
-    <div class="titleSection">
-        <h2 class="montserratBold titleColor">Mon profil.</h2>
+<section class='profileSection robotoRegular'>
+    <div class="titleDiv">
+        <h2 class="montserratBold titleColor">Mon profil</h2>
     </div>
-    <div class='navBarProfil'>
-        <a href="/profile">Mon compte</a>
-        <a href="/myRidesharing">Mes trajets</a>
+
+    <div class='navBarProfil robotoBold'>
+        <div id="compteBtn" class="compteBtn">
+            <a href="/profile">Mon compte</a>
+        </div>
+        <div id="myRideBtn" class="myRideBtn">
+            <a href="/myRidesharing">Mes trajets</a>
+        </div>        
     </div>
+
     <div class='userCard'>
         <div class ='picture'>
             <img src="" alt="photo de profil">
@@ -13,31 +19,62 @@
         <div class='userInfo'>
             <p class='pseudo'>testPseudo</p>
             <p class='creationDate'>29 octobre 2025</p>
-            <p class='creditBalance'>35 crédits</p>
+            <p class='creditBalance'><span class="robotoBold">35 </span>crédits</p>
         </div>
     </div>
+
     <div class="myCars">
-        <ul>
+        <ul class="listCars">
             <li>
-                <span class="material-symbols-outlined">directions_car</span>
-                <p>Dacia Duster</p>
-                <p>4 places</p> 
-                <span class="material-symbols-outlined">edit</span>
+                <div class="carLogo">
+                    <span class="material-symbols-outlined">directions_car</span>
+                </div>
+                <div class ="brandCar">
+                    <p>Dacia</p>
+                </div>
+                <div class="modelCar">
+                    <p>Duster</p>
+                </div>
+                <div class="Seats">
+                    <p>4 p.</p>
+                </div>
+                <div class="energyType">
+                    <span class="material-symbols-outlined">local_gas_station</span>
+                </div>
+                <div class="editCar">
+                    <span class="material-symbols-outlined">edit</span>
+                </div>    
             </li>
             <li>
-                <span class="material-symbols-outlined">directions_car</span>
-                <p>Audi e-tron</p>
-                <p>1 place</p>
-                <span class="material-symbols-outlined">electric_bolt</span>
-                <span class="material-symbols-outlined">edit</span>
+                <div class="carLogo">
+                    <span class="material-symbols-outlined">directions_car</span>
+                </div>
+                <div class ="brandCar">
+                    <p>Audi</p>
+                </div>
+                <div class="modelCar">
+                    <p>e-tron</p>
+                </div>
+                <div class="Seats">
+                    <p>1 p.</p>
+                </div>
+                <div class="energyType">
+                    <span class="material-symbols-outlined">electric_bolt</span>
+                </div>
+                <div class="editCar">
+                    <span class="material-symbols-outlined">edit</span>
+                </div>    
             </li>
         </ul>
-        <a href="addCar">
-            <span class="material-symbols-outlined">directions_car</span>
-            <span class="material-symbols-outlined">add</span>
-        </a> 
+        <div class="addCarBtn">
+            <a href="addCar">
+                <span class="material-symbols-outlined">directions_car</span>
+                <span class="material-symbols-outlined">add</span>
+            </a>  
+        </div>        
     </div>
+
     <div class='newRide'>
-        <a href="/showCreateRidesharing" class="newRideBtn">Proposer un trajet.</a>
+        <a href="/showCreateRidesharing" class="newRideBtn robotoBold">Proposer un trajet.</a>
     </div>
 </section> 
