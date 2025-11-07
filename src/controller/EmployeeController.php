186ccdfc18c7d969e77c428fe2d2f1b80e843cae
Marc <marc.uny39@gmail.com>
2025-11-07 11:14:40 +0100

    feat(EmployeeController): Ajout de la méthode pour rechercher les détails d'une participation depuis un avis.

--- src/controller/EmployeeController.php
+++ src/controller/EmployeeController.php
@@ -31,8 +31,42 @@ class EmployeeController extends BaseController
         ]);
     }
 
-    public function showDetailReview(string $id):void
+    public function findDetailReview(int $idParticipation, int $idTarget, int $idRedactor):array
     {
-        
+        $data = [];
+
+        try{
+            $participate = $this->participateRepo->findById($idParticipation);
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche de participation : " . $e->getMessage());
+        }
+
+        $data [] = $participate;
+
+        try{
+            $ride = $this->ridesharingRepo->findById($participate->getIdRidesharing());
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche du trajet : " . $e->getMessage());
+        }
+
+        $data[] = $ride;
+
+        try{
+            $driver = $this->userRepo->findById($idTarget);
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche du chauffeur : " . $e->getMessage());
+        }
+
+        $data[] = $driver;
+
+        try{
+            $passanger = $this->userRepo->findById($idRedactor);
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche du chauffeur : " . $e->getMessage());
+        }
+
+        $data[]= $passanger;
+
+        return $data;       
     }
 }
