b138bb1f4e5ac30f3d19792255e54b89fcfaf3c0
Marc <marc.uny39@gmail.com>
2025-11-09 10:34:44 +0100

    refact(ReviewController): La méthode findDetailReview est déplace d'EmployeeController vers ReviewController, car il est question de review.

--- src/controller/EmployeeController.php
+++ src/controller/EmployeeController.php
@@ -30,43 +30,4 @@ class EmployeeController extends BaseController
             'countReview' => $countReview
         ]);
     }
-
-    public function findDetailReview(int $idParticipation, int $idTarget, int $idRedactor):array
-    {
-        $data = [];
-
-        try{
-            $participate = $this->participateRepo->findById($idParticipation);
-        }catch(\Exception $e){
-            $this->logger->log('ERROR', "Erreur lors de la recherche de participation : " . $e->getMessage());
-        }
-
-        $data [] = $participate;
-
-        try{
-            $ride = $this->ridesharingRepo->findById($participate->getIdRidesharing());
-        }catch(\Exception $e){
-            $this->logger->log('ERROR', "Erreur lors de la recherche du trajet : " . $e->getMessage());
-        }
-
-        $data[] = $ride;
-
-        try{
-            $driver = $this->userRepo->findById($idTarget);
-        }catch(\Exception $e){
-            $this->logger->log('ERROR', "Erreur lors de la recherche du chauffeur : " . $e->getMessage());
-        }
-
-        $data[] = $driver;
-
-        try{
-            $passanger = $this->userRepo->findById($idRedactor);
-        }catch(\Exception $e){
-            $this->logger->log('ERROR', "Erreur lors de la recherche du chauffeur : " . $e->getMessage());
-        }
-
-        $data[]= $passanger;
-
-        return $data;       
-    }
 }
--- src/controller/ReviewController.php
+++ src/controller/ReviewController.php
@@ -79,4 +79,48 @@ class ReviewController extends BaseController
         exit;
         
     }
+
+    public function findDetailReview(int $idParticipation, int $idTarget, int $idRedactor):array
+    {
+        $data = [];
+
+        try{
+            $participate = $this->participateRepo->findById($idParticipation);
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche de participation : " . $e->getMessage());
+        }
+
+        $data [] = $participate;
+
+        try{
+            $ride = $this->ridesharingRepo->findById($participate->getIdRidesharing());
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche du trajet : " . $e->getMessage());
+        }
+
+        $data[] = $ride;
+
+        try{
+            $driver = $this->userRepo->findById($idTarget);
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche du chauffeur : " . $e->getMessage());
+        }
+
+        $data[] = $driver;
+
+        try{
+            $passanger = $this->userRepo->findById($idRedactor);
+        }catch(\Exception $e){
+            $this->logger->log('ERROR', "Erreur lors de la recherche du chauffeur : " . $e->getMessage());
+        }
+
+        $data[]= $passanger;
+
+        return $data;       
+    }
+
+    public function countReviewValidateByDay():int
+    {
+        $listeReviewValidate = $this->reviewRepo->findByStatus(StatusReview::approved);
+    }
 }
