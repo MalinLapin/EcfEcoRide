a4fbd2e503587b8e4913f65540211d80fc4c9e66
Marc <marc.uny39@gmail.com>
2025-11-12 11:01:22 +0100

    refact(Role): Comme pour l'énum StatusReview je crée une classe spécifique afin que l'autoloader puisse gérer l'instanciation de l'énum.

---
+++ src/model/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\model;
+
+/**
+ * Enumération pour représenter les différents rôles d'un utilisateur.
+ * Cette énumération permet de définir clairement les rôles possibles d'un utilisateur,
+ * ce qui aide à éviter les erreurs de typage et de logique dans le code.
+ */
+enum Role: string
+{
+        case user = 'user'; // Rôle par défaut pour les utilisateurs normaux.
+        case admin = 'admin'; // Rôle pour les administrateurs du système.
+        case employe = 'employe';// Rôle pour les employés du service client ou de la gestion des covoiturages.
+}
--- src/model/UserModel.php
+++ src/model/UserModel.php
@@ -5,18 +5,8 @@ namespace App\model;
 
 use DateTimeImmutable;
 use App\security\Validator;
+use App\model\Role;
 
-/**
- * Enumération pour représenter les différents rôles d'un utilisateur.
- * Cette énumération permet de définir clairement les rôles possibles d'un utilisateur,
- * ce qui aide à éviter les erreurs de typage et de logique dans le code.
- */
-enum Role: string
-{
-        case user = 'user'; // Rôle par défaut pour les utilisateurs normaux.
-        case admin = 'admin'; // Rôle pour les administrateurs du système.
-        case employe = 'employe';// Rôle pour les employés du service client ou de la gestion des covoiturages.
-}
 
 /**
  * Fichier contenant notre classe UserModel qui étend la classe BaseModel.
